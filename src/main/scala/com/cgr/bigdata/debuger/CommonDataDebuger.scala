9bff242d97ed31cce2e503d7f7852dcaf4f7ea6c
guorui.chen <guorui.chen@bestwehotel.com>
2017-03-29 14:23:47 +0800

    add DataDebuger

---
+++ src/main/scala/com/cgr/bigdata/debuger/CommonDataDebuger.scala
@@ -0,0 +1,17 @@
+package com.cgr.bigdata.debuger
+
+/**
+  * Created by guorui.chen on 2017/3/29.
+  */
+class CommonDataDebuger(keys:List[String]) extends DataDebuger{
+
+
+
+  override def debug(): Unit = {
+
+  }
+
+  private def sceneRestore(): Unit ={
+
+  }
+}
---
+++ src/main/scala/com/cgr/bigdata/debuger/DataDebuger.scala
@@ -0,0 +1,10 @@
+package com.cgr.bigdata.debuger
+
+/**
+  * Created by guorui.chen on 2017/3/29.
+  */
+trait DataDebuger {
+
+  def debug():Unit
+
+}
