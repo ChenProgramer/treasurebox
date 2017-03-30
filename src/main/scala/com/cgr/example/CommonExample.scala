bfdd96cdcbbdfff11e8c4cab7a2eeb35a27ecffe
guorui.chen <guorui.chen@bestwehotel.com>
2017-03-30 18:19:41 +0800

    add example

---
+++ src/main/scala/com/cgr/example/CommonExample.scala
@@ -0,0 +1,12 @@
+package com.cgr.example
+
+/**
+  * Created by guorui.chen on 2017/3/30.
+  */
+object CommonExample {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello TreasureBox")
+  }
+
+}
